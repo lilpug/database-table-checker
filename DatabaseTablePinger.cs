f4942558dc76d35806aee75bb700aa4ce84a1b6f
David Whitehead <david__whitehead2@hotmail.com>
2017-10-07 15:42:13 +0100

    Update DatabaseTablePinger.cs

--- DatabaseTablePinger.cs
+++ DatabaseTablePinger.cs
@@ -1,39 +1,39 @@
-﻿using DotNetSDB;
+using DotNetSDB;
 using System;
 using System.Diagnostics;
 
 namespace Database.Testing
 {
-    /// <summary>
-    /// A quick diagnostic class to log if a databases tables are being slow
-    /// </summary>
-    public static class DatabaseTablePinger
-    {
-        /// <summary>
-        /// This function creates a log table that will then be used by the RunPinger function
-        /// </summary>
-        /// <param name="loggerDB"></param>
+	/// <summary>
+	/// A quick diagnostic class to log if a databases tables are being slow
+	/// </summary>
+	public static class DatabaseTablePinger
+	{
+		/// <summary>
+		/// This function creates a log table that will then be used by the RunPinger function
+		/// </summary>
+		/// <param name="loggerDB"></param>
 		public static void CreateLogTable(SQLServer2016 loggerDB)
 		{	
-            if (!loggerDB.table_exist("log"))
-            {
-                loggerDB.add_create_table("log", new string[] { "id", "value", "generated" },
-                                                    new string[] { "int Identity(1,1) PRIMARY KEY not null", "varchar(max)", "datetime not null" });
-                loggerDB.run();
-            }            
+		    if (!loggerDB.table_exist("log"))
+		    {
+			loggerDB.add_create_table("log", new string[] { "id", "value", "generated" },
+							 new string[] { "int Identity(1,1) PRIMARY KEY not null", "varchar(max)", "datetime not null" });
+			loggerDB.run();
+		    }            
 		}
 
-        /// <summary>
-        /// This function runs a basic query against all tables within a database and logs any slow calls
-        /// </summary>
-        /// <param name="loggerDB">The database object for the logger db and table location</param>
-        /// <param name="checkerDB">The database object for the database to be checked</param>
-        /// <param name="databaseName">The database name that you want to check</param>
-        public static void RunPinger(SQLServer2016 loggerDB, SQLServer2016 checkerDB, string databaseName)
-        {   
-            checkerDB.add_select("INFORMATION_SCHEMA.TABLES", "TABLE_NAME");
-            checkerDB.add_where_normal("INFORMATION_SCHEMA.TABLES", "TABLE_TYPE", "BASE TABLE");
-            checkerDB.add_where_normal("INFORMATION_SCHEMA.TABLES", "TABLE_CATALOG", databaseName);
+		/// <summary>
+		/// This function runs a basic query against all tables within a database and logs any slow calls
+		/// </summary>
+		/// <param name="loggerDB">The database object for the logger db and table location</param>
+		/// <param name="checkerDB">The database object for the database to be checked</param>
+		/// <param name="databaseName">The database name that you want to check</param>
+		public static void RunPinger(SQLServer2016 loggerDB, SQLServer2016 checkerDB, string databaseName)
+		{   
+			checkerDB.add_select("INFORMATION_SCHEMA.TABLES", "TABLE_NAME");
+			checkerDB.add_where_normal("INFORMATION_SCHEMA.TABLES", "TABLE_TYPE", "BASE TABLE");
+			checkerDB.add_where_normal("INFORMATION_SCHEMA.TABLES", "TABLE_CATALOG", databaseName);
 			string[] tables = checkerDB.run_return_string_array();
 
 			foreach(string table in tables)
@@ -46,7 +46,7 @@ namespace Database.Testing
 					// Begin timing.
 					stopwatch.Start();
 
-                    checkerDB.add_pure_sql($"select top 1 1 from {table}");                            
+					checkerDB.add_pure_sql($"select top 1 1 from {table}");                            
 					string temp = checkerDB.run_return_string();
 
 					// Stop timing.
@@ -60,7 +60,7 @@ namespace Database.Testing
 				catch (Exception e)
 				{
 					string error = $"Table: {table}{Environment.NewLine}Error: {e.Message}";
-						
+
 					loggerDB.add_insert("log", new object[] { error, DateTime.Now });
 					loggerDB.run();                                						
 				}
@@ -70,6 +70,6 @@ namespace Database.Testing
 					stopwatch = null;
 				}
 			}			
-        }
-    }
+		}
+	}
 }
